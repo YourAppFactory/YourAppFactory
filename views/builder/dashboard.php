e39b73b4502b382e1adf801acce18cc24e27580a
anamariaaya <39868590+anamariaaya@users.noreply.github.com>
2024-12-07 21:06:51 +0100

    fix css

--- views/builder/dashboard.php
+++ views/builder/dashboard.php
@@ -3,7 +3,7 @@
     <section class="builder__grid m-t-5">
         <a href="/builder/apps" class="builder__grid__card">
             <img class="builder__grid__card__image" src="/build/img/icon-builder-apps.svg" alt="{%builder_creation_app_title%}">
-            <h2 class="builder__builder__grid__title">{%builder_creation_app_title%}</h2>
+            <h2 class="builder__grid__card__title">{%builder_creation_app_title%}</h2>
             <p class="builder__grid__card__text">{%builder_creation_app_text%}</p>
         </a>
         <a href="/builder/funnels" class="builder__grid__card">
