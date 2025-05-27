7211f3e32cab2bcecd38dbc78fdd3a323ff42588
Ana María Aya <39868590+anamariaaya@users.noreply.github.com>
2025-05-27 07:43:09 +0200

    Start pricing page

---
+++ views/pages/pricing.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <h1 class="center w-100">{%pricing_main_title%}</h1>
+
+    <p class="center">{%pricing_text_1%}</p>
+    <a href="https://calendar.app.google/j54oswg4dHALhFjr5" class="btn-submit block w-100 center m-t-3" target="_blank">{%pricing_link_1%}</a>
+
+    <h2 class="m-t-5 center">{%pricing_section_subtitle_1%}</h2>
+</div>
