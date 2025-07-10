7a41fd8edcef7703f66dc055684f6ff5ac49c131
anamariaaya <39868590+anamariaaya@users.noreply.github.com>
2025-07-10 19:15:43 +0200

    change whatsapp redirections in promo and button

--- views/templates/promo.php
+++ views/templates/promo.php
@@ -1,9 +1,13 @@
+<?php
+ $lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : 'en';
+?>
+
 <div class="modal-promo--active">
     <div class="modal-promo">
         <img class="modal-promo__img only--desktop" src="/build/img/<?php echo $promo->promo;?>" alt="Promo">
         <img class="modal-promo__img only--mobile" src="/build/img/<?php echo $promo->promo_mobile;?>" alt="Promo">
         <div class="modal-promo__buttons">
-            <a href="/pricing" class="modal-promo__btn modal-btn">{%promo_contact_btn%}</a>
+            <a href="<?php echo $lang == 'es' ? "https://wa.me/12028679694?text=%C2%A1Lo%20quiero!%20SUMMER%20CHALLENGE.%20%C2%A1Vamos" : "https://wa.me/12028679694?text=Hi!%20I'd%20love%20to%20claim%20my%20spot%20in%20the%20SUMMER%20CHALLENGE." ?>" class="modal-promo__btn modal-btn">{%promo_contact_btn%}</a>
             <button class="modal-promo__btn--cancel modal-btn">{%promo_close_btn%}</button>
         </div>
         <div class="modal-promo__btn--close modal-btn">
