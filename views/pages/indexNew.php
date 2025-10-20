52a5b28660719124a1e3107484617796cc0cbf4b
anamariaaya <39868590+anamariaaya@users.noreply.github.com>
2025-10-20 21:03:43 +0200

    Update images new page

--- views/pages/indexNew.php
+++ views/pages/indexNew.php
@@ -10,19 +10,19 @@
 
     <div class="main-2__cards">
         <div class="main-2__card card--top">
-            <img class="main-2__card__icon" src="https://www.shutterstock.com/image-photo/lock-phone-password-mobile-cybersecurity-600w-2461564955.jpg" alt="">
+            <img class="main-2__card__icon" src="/build/img/img-digital.png" alt="Image Digital transformation">
             <h3 class="main-2__card__title">Digital Transformation</h3>
             <p class="main-2__card__description">Empowering businesses to leverage digital technologies for improved efficiency and growth.</p>
             <a href="/digital-transformation" class="main-2__card__link">Transform your business</a>
         </div>
         <div class="main-2__card card--center">
-            <img class="main-2__card__icon" src="https://www.shutterstock.com/image-photo/event-development-seminar-business-people-600w-2485558515.jpg" alt="App Development Icon">
+            <img class="main-2__card__icon" src="/build/img/img-training.png" alt="Training Image">
             <h3 class="main-2__card__title">Training</h3>
             <p class="main-2__card__description">Comprehensive training programs to upskill your team and drive innovation.</p>
             <a href="/training" class="main-2__card__link">Learn More</a>
         </div>
         <div class="main-2__card card--bottom">
-            <img class="main-2__card__icon" src="https://www.shutterstock.com/image-photo/people-group-business-meeting-analytics-600w-2475089495.jpg" alt="UI/UX Design Icon">
+            <img class="main-2__card__icon" src="/build/img/img-consulting.png" alt="Business Consulting Image">
             <h3 class="main-2__card__title">Business Consulting</h3>
             <p class="main-2__card__description">Expert guidance to help businesses optimize their processes and achieve their goals.</p>
             <a href="/business-consulting" class="main-2__card__link">Get Started</a>
